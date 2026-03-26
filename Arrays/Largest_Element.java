697f94548db4e18fdd4c4bcfa854b3573ae189c8
KabirPandey <kabirpandey.career@gmail.com>
2026-03-26 15:08:11 +0530

    code commited

---
+++ Arrays/Largest_Element.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class Largest_Element {
+    public static void main(String[] args){
+
+        int[] arr = {1,4,3,5,6,7,9};
+        System.out.println(maxelement(arr));
+
+        
+    }
+
+    static int maxelement(int[] arr ){
+
+            int max = arr[0];
+
+            for(int i = 1; i < arr.length; i++){
+                if(max<arr[i]){
+
+                    max = arr[i];
+                }
+
+            
+            }
+            return max;
+
+
+        }
+}
